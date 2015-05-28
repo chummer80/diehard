442a113179f04929ee7f331a80c6762983a342a5
Mike Chu <mike.c.chu@gmail.com>
2015-05-28 22:30:26 +0000

    make die hard logos transparent, play sound when brand is clicked in the nav bar

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,5 @@
+var brandLink = document.querySelector('#brand_link');
+var yippeeSound = document.querySelector('#yippee_sound');
+brandLink.addEventListener('click', function() {
+	yippeeSound.play();
+});
